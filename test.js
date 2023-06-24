a22bcd7b1fdcc7608130ad62413a20f1792fe9b5
ahmadshahdonishyar <ahmadshahdonishyar@gmail.com>
2023-06-24 14:23:57 +0430

    input bug fixed

--- test.js
+++ test.js
@@ -11,26 +11,40 @@ var randNumber= Math.floor(Math.random() * 100 +1);
 function getUser_input(){
 let user_input = document.getElementById("user__input").value;
 
-    if(randNumber<user_input){
-       let p = document.createElement("p");
-        p.innerHTML = "Your guess is too high !";
-        p.style.color="red";
-        p.style.fontWeight=700;
-        document.body.appendChild(p);
+    if(Number.isInteger(parseInt(user_input))){
 
-    }else if(randNumber>user_input){
+   
+
+        if(randNumber<user_input ){
         let p = document.createElement("p");
-        p.innerHTML = "Your guess is too low !";
-        p.style.color="#ff9900";
-        p.style.fontWeight=700;
-        document.body.appendChild(p);
+            p.innerHTML = "Your guess is too high !";
+            p.style.color="red";
+            p.style.fontWeight=700;
+            document.body.appendChild(p);
+
+        }else if(randNumber>user_input ){
+            let p = document.createElement("p");
+            p.innerHTML = "Your guess is too low !";
+            p.style.color="#ff9900";
+            p.style.fontWeight=700;
+            document.body.appendChild(p);
+
+        }else{
+            let p = document.createElement("p");
+            p.innerHTML = "Your guess is correct";
+            p.style.color="green";
+            p.style.fontWeight=700;
+            document.body.appendChild(p);
+        }
 
     }else{
+
         let p = document.createElement("p");
-        p.innerHTML = "Your guess is correct";
-        p.style.color="green";
+        p.innerHTML = "Your input is wrong !";
+        p.style.color="#black";
         p.style.fontWeight=700;
         document.body.appendChild(p);
+
     }
 
     
